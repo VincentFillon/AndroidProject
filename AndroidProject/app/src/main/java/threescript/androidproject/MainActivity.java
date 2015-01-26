01e66f32b73eb2f46f5adfec333072483214190d
Valentin PASSE <valentin-passe@hotmail.fr>
2015-01-26 09:04:01 +0100

    commit

--- AndroidProject/app/src/main/java/threescript/androidproject/MainActivity.java
+++ AndroidProject/app/src/main/java/threescript/androidproject/MainActivity.java
@@ -1,9 +1,12 @@
 package threescript.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +15,14 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button PlayButton = (Button) findViewById(R.id.btnPlay);
+        PlayButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, game.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
